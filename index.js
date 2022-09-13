553331a8e8b1666b08b66c48f5b6364847c93493
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2022-09-13 22:59:38 +0500

    New 2nd JS

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ let screenValue = '';
 for (item of buttons) {
     item.addEventListener('click', (e) => {
         buttonText = e.target.innerText;
-        console.log('Button text is ', buttonText);
         if (buttonText == 'X') {
             buttonText = '*';
             screenValue += buttonText;
